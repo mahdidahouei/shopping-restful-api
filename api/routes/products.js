b9349bb718a3e84670d8222c41b5918296a8d803
Mahdi <mahdi.a.200066@gmail.com>
2020-04-02 16:38:32 +0430

    Added orders route

--- api/routes/products.js
+++ api/routes/products.js
@@ -13,4 +13,28 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') res.status(200).json({
+        message: 'You discovered the special ID',
+        id: id,
+    });
+    else res.status(200).json({
+        message: `you passed ${id} id`
+    });
+});
+
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product',
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product',
+    });
+});
+
 module.exports = router;
--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
