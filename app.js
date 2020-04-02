3c6e690c9668aa9149c63489d314a8199080e519
Mahdi <mahdi.a.200066@gmail.com>
2020-04-02 16:54:55 +0430

    Did some error handling!

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not fucking found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        }
+    })
+});
+
 module.exports = app;
